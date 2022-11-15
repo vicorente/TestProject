13d12700945e3ae69f3e88212467647535d14415
trucodevs <trucodevs@gmail.com>
2022-11-15 19:47:34 +0100

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Víctor González Llorente on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
